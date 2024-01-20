a4d5a64aa3da9f44eb1052d5018a4d3629a85669
Gomes Cabral <phgcabral@hotmail.com>
2024-01-20 17:48:21 +0100

    send email at 10:30

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import yagmail
 import pandas
 from news import NewsFeed
 import datetime
+import time
 
 
 df = pandas.read_excel('people.xlsx')
 
-for index, row in df.iterrows():
-    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-    today = datetime.datetime.now().strftime('%Y-%m-%d')
-    newsFeed = NewsFeed(row['interest'], 
+while True:
+    for index, row in df.iterrows():
+        if datetime.datetime.now().hour == 10 and datetime.datetime.now().minute == 30:
+            yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+            today = datetime.datetime.now().strftime('%Y-%m-%d')
+
+            newsFeed = NewsFeed(row['interest'], 
                         from_date=yesterday, 
                         to_date=today)
-    email = yagmail.SMTP(user="phgcabral8@gmail.com", password="zswyertmbzvmjymw")
-    email.send(to=row['email'],
+            email = yagmail.SMTP(user="your@gmail.com", password="yourPassword")
+            email.send(to=row['email'],
            subject=f"Your {row['interest']} news for today",
            contents=f"Hi {row['name']}\n See what's on about {row['interest']} today. {newsFeed.get()}")
+        time.sleep(60)
