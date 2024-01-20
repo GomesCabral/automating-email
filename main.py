be30d49beabd6ed19068d9db5756a54e6686f7f8
Gomes Cabral <phgcabral@hotmail.com>
2024-01-20 17:07:01 +0100

    use yagmail and pandas

--- main.py
+++ main.py
@@ -1,33 +1,13 @@
-import requests
+import yagmail
+import pandas
+from news import NewsFeed
 
 
-# url = "https://newsapi.org/v2/everything?q=bitcoin&language=en&apiKey=837db0c0c7e84c4282bb150742cc12f7"
+df = pandas.read_excel('people.xlsx')
 
-class NewsFeed:
-    """Representing multiple nesw title and link as a sigle string"""
-
-    base_url = "https://newsapi.org/v2/everything?"
-    api_key = "837db0c0c7e84c4282bb150742cc12f7"
-
-    def __init__(self, interest, from_date, to_date, language):
-        self.interest = interest
-        self.from_date = from_date
-        self.to_date = to_date
-        self.language = language
-        
-    def get(self):
-        url = f"{self.base_url}qInTitle={self.interest}&from={self.from_date}&to={self.to_date}&language={self.language}&apiKey={self.api_key}"
-
-        response = requests.get(url)
-        content = response.json()
-        articles = content['articles']
-
-        email_body = ""
-        for article in articles:
-            email_body = email_body + article['title'] + "\n" + article['url'] + "\n" + "\n"
-
-        return email_body
-    
-
-newsFeed = NewsFeed("bitcoin", "2024-01-01", "2024-01-20", "pt")
-print(newsFeed.get())
+for index, row in df.iterrows():
+    newsFeed = NewsFeed(row['interest'], from_date="2024-01-01", to_date="2024-01-19")
+    email = yagmail.SMTP(user="phgcabral8@gmail.com", password="zswyertmbzvmjymw")
+    email.send(to=row['email'],
+           subject=f"Your {row['interest']} news for today",
+           contents=f"Hi {row['name']}\n See what's on about {row['interest']} today. {newsFeed.get()}")
